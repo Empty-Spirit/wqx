3aa8790aebe1b207c5bf14171a37e6c12d234b4d
baiyi <baiyi@tsign.cn>
2021-07-02 15:14:20 +0800

    登录页面优化

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="login">
-    <van-form @submit="onSubmit">
+    <img class="userimg" src="@/assets/img/userLogo.jpg" alt="" />
+    <van-form class="userform" @submit="onSubmit">
       <van-field
         v-model="form.user_name"
         name="user_name"
@@ -22,52 +23,79 @@
         >
       </div>
     </van-form>
+    <span class="userpwd">忘记密码</span>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import { useStore } from "vuex"
-import { useRouter } from 'vue-router'
-import { Notify } from "vant"
-import api from '@/config/api'
+import { defineComponent, ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import { Toast } from "vant";
+import api from "@/config/api";
 
 export default defineComponent({
-  setup () {
+  setup() {
     let store = useStore();
     let router = useRouter();
 
     let form = ref({
       user_name: "张哲学",
       user_pwd: "147zhang",
-    })
+    });
 
-    let onSubmit = (form) => {
+    let onSubmit = form => {
+      Toast.loading({
+        message: "加载中...",
+        forbidClick: true,
+      });
       api.user.login(form).then(res => {
         if (res) {
           // 将用户信息存储在store中
-          store.commit('upDateUserInfo', res)
+          store.commit("upDateUserInfo", res);
           // 对存储在cookie中的token进行拼接
-          let str = 'token=' + res.token + '=token'
+          let str = "token=" + res.token + "=token";
           // 存储cookie
-          document.cookie = str
+          document.cookie = str;
           // 登录成功提示
-          Notify({ type: 'success', message: '登录成功' });
-          // 登录成功跳转 
+          Toast.success("登录成功");
+          // 登录成功跳转
           router.push({
-            name: "Main"
-          })
+            name: "Main",
+          });
         }
-      })
-    }
+      });
+    };
 
     return {
       form,
-      onSubmit
-    }
+      onSubmit,
+    };
   },
-})
+});
 </script>
 
 <style lang="less" scoped>
+.login {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .userimg {
+    width: 100px;
+    height: 100px;
+    margin-bottom: 100px;
+    border: 1px solid;
+    border-radius: 50%;
+  }
+  .userform {
+    width: 100%;
+    margin-bottom: 50px;
+  }
+  .userpwd {
+    font-size: 13px;
+    text-decoration: underline;
+  }
+}
 </style>
